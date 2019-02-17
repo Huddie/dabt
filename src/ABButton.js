4c3287683ac9b2d0c01c9a8f64a7a4c251724f78
Julian Moskovits <julianmoskovits@Julians-MacBook-Pro.local>
2019-02-17 15:34:47 -0500

    debugging

--- src/ABButton.js
+++ src/ABButton.js
@@ -6,21 +6,6 @@ class ABButton extends Component {
 
   constructor(props) {
     super(props)
-    this.generateStyle = this.generateStyle.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.logview = this.logview.bind(this);
-    this.loginteraction = this.loginteraction.bind(this);
-
-    var core =  new ABCore()
-    this.state = {
-      core: core,
-      id: this.props.id,
-      backgroundColor: core.generateColor(),
-      textColor: core.generateColor(),
-      cornerRadius: core.generateNumber(0, 20),
-      fontSize: core.generateNumber(0, 100)
-
-    };
     var config = {
       apiKey: "AIzaSyD22AIhw7wfs6gCNMZdC2gXnYmWpI1_veY",
       authDomain: "jhacks-7c79b.firebaseapp.com",
@@ -30,6 +15,24 @@ class ABButton extends Component {
       messagingSenderId: "487556590025"
     };
     firebase.initializeApp(config);
+    this.generateStyle = this.generateStyle.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.logview = this.logview.bind(this);
+    this.loginteraction = this.loginteraction.bind(this);
+
+    var core =  new ABCore()
+    var styleGuide = core.getStyleID(this.props.id)
+    console.log(styleGuide)
+    this.state = {
+      core: core,
+      id: this.props.id,
+      backgroundColor: styleGuide.color,
+      textColor: 'white',
+      cornerRadius: styleGuide.radius,
+      fontSize: styleGuide.size
+
+    };
+
   }
 
   componentDidMount() {
--- src/ABCore.js
+++ src/ABCore.js
@@ -1,7 +1,161 @@
 import { normalize } from "path";
+import React, { Component } from 'react';
 
 var firebase = require("firebase"); 
-class ABCore {
+class ABCore extends Component{
+
+    constructor(props){
+        super(props)
+        this.getStyleID = this.getStyleID.bind(this);
+        var styles = {
+            backgroundColors : [
+                'yellow',
+                'red',
+                'green',
+                'blue',
+                'black',
+                'purple',
+                'orange'
+            ],
+
+            fontSizes : [
+                10,
+                20,
+                30,
+                40,
+                50,
+                60
+            ],
+
+            borderRadiuss : [
+                3,
+                6,
+                9,
+                12,
+                15,
+                18
+            ]
+        }
+
+        this.state = {
+            s : styles
+        }
+    }
+    
+
+
+
+    getStyleID = (id) => {
+        firebase.database().ref('records').once('value').then(function(snapshot){
+            //if database is empty populate it with all options
+            console.log(this.state)
+            if(snapshot.val() == undefined || snapshot.val() == null){
+                for(var color in this.state.s['backgroundColors']){
+                    firebase.database().ref(`records/classes/${id}/preferences/backgroundColor/${color}`).set({interactions: 0, views: 1});
+                }
+                for(var size in this.state.s['fontSizes']){
+                    firebase.database().ref(`records/classes/${id}/preferences/fontSize/${size}`).set({interactions: 0, views: 1});
+                }
+                for(var radius in this.state.s['borderRadiuss']){
+                    firebase.database().ref(`records/classes/${id}/preferences/borderRadius/${radius}`).set({interactions: 0, views: 1});
+                }
+            }
+
+            else{
+                //choose a color
+                var color_total = 0;
+                var size_total = 0;
+                var radius_total = 0;
+                var selected_color = undefined;
+                var selected_size = undefined;
+                var selected_radius = undefined;
+                var color_probabilities = {};
+                var size_probabilities = {};
+                var radius_probabilities = {};
+
+                firebase.database.ref(`records/classes/${id}/preferences`).once('value').then(function(snapshot){
+                    var backgroundColors = snapshot.val()['backgroundColor']
+                    for(var color in backgroundColors){
+                        color_probabilities[color] = Math.pow( (backgroundColors[color]['interactions'] + 1.0)  / (backgroundColors[color]['views'] + 1.0) , 2 )
+                        color_total += color_probabilities[color]
+                    }
+
+                    var fontSizes = snapshot.val()['fontSize']
+                    for(var size in fontSizes){
+                        size_probabilities[size] = Math.pow( (fontSizes[size]['interactions'] + 1.0)  / (fontSizes[size]['views'] + 1.0) , 2)
+                        size_total += size_probabilities[size]
+                    }
+
+                    var borderRadiuss = snapshot.val()['borderRadius']
+                    for(var radius in borderRadiuss){
+                        radius_probabilities[radius] = Math.pow( (borderRadiuss[radius]['interactions'] + 1.0)  / (borderRadiuss[radius]['views'] + 1.0) , 2)
+                        radius_total += radius_probabilities[radius]
+                    }
+
+                    //Normalize probabilities
+                    for(var color in color_probabilities){
+                        color_probabilities[color] /= color_total
+                    }
+                    for(var size in size_probabilities){
+                        size_probabilities[size] /= size_total
+                    }                    
+                    for(var color in color_probabilities){
+                        radius_probabilities[radius] /= radius_total
+                    }
+
+                    //select values based on probabilities
+                    var remaining_probability = 1.0;
+                    for(var color in color_probabilities){
+                        if(Math.random() < color_probabilities[color] / remaining_probability){
+                            selected_color = color;
+                            break;
+                        }
+                        else{
+                            remaining_probability -= color_probabilities[color];
+                        }
+                    }
+                    if(selected_color == undefined){
+                        selected_color = backgroundColors[0];
+                    }
+
+                    remaining_probability = 1.0;
+                    for(var size in size_probabilities){
+                        if(Math.random() < size_probabilities[size] / remaining_probability){
+                            selected_size = size;
+                            break;
+                        }
+                        else{
+                            remaining_probability -= size_probabilities[size];
+                        }
+                    }
+                    if(selected_size == undefined){
+                        selected_size = fontSizes[0];
+                    }
+
+                    remaining_probability = 1.0;
+                    for(var radius in radius_probabilities){
+                        if(Math.random() < radius_probabilities[radius] / remaining_probability){
+                            selected_radius = radius;
+                            break;
+                        }
+                        else{
+                            remaining_probability -= radius_probabilities[radius];
+                        }
+                    }
+                    if(selected_radius == undefined){
+                        selected_radius = borderRadiuss[0];
+                    }
+
+                });
+            }
+            return {
+                color : selected_color,
+                size : selected_size,
+                radius : selected_radius
+            }
+        });
+        
+    }
 
     generateColor() {
         var colors = [
@@ -20,13 +174,13 @@ class ABCore {
         return Math.floor(Math.random() * hi) + low;
     }
     logView(id, key, value) {
-        firebase.database().ref(`records/classes/${id}/preferences/${key}/${value}`).once('value').then(function(snapshot) {
+        var path = `records/classes/${id}/preferences/${key}/${value}`
+        firebase.database().ref(path).once('value').then(function(snapshot) {
             if(snapshot.val() == undefined || snapshot.val() == null) {
-                firebase.database().ref(key).set({interactions: 0, views: 1});
-                this.normalizeProbs(id, key, value);
+                firebase.database().ref(path).set({interactions: 0, views: 1});
             } else {
                 var dict = snapshot.val()
-                firebase.database().ref(key).update({views: dict['views']+1});
+                firebase.database().ref(path).update({views: dict['views']+1});
             }
         }); 
     }
@@ -37,9 +191,6 @@ class ABCore {
         });
     }
 
-    normalizeProbs(id, key, value) {
-
-    }
 }
   
 export default ABCore;
